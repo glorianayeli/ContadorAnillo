68435d285209717bdc285b0be6fe9630e801071d
LizZaor <42955636+LizZaor@users.noreply.github.com>
2019-02-28 21:18:16 -0600

    Revert "Modificado 3.0"
    This reverts commit 3d11aaf0dfb0c0ecd2272c872951a943e98b515f.

--- contador.c
+++ contador.c
@@ -6,7 +6,7 @@ void main(void) {
     set_tris_d(0x00);
     output_d(0x00);
     int16 conteo;
-    while (TRUE) {
+    while (1) {
         /*output_d(0x01);
         delay_ms(500);
         output_d(0x02);
@@ -23,7 +23,7 @@ void main(void) {
         delay_ms(500);
         output_d(0x80);
         delay_ms(500);*/
-        for(conteo=128;conteo>=128;conteo/=2)
+        for(conteo=1;conteo<=128;conteo/=2)
         {
             output_d(conteo);
             delay_ms(500);
